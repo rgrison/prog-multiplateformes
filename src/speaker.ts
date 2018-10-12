d8a001f187c20929556730a858c6921f07ec75d6
Rémi Grison <remi@dhcp-191-138.eduroam.emn.fr>
2018-10-12 14:10:43 +0200

    updated Speaker class (constructor takes only JSON as parameter)

--- src/speaker.ts
+++ src/speaker.ts
@@ -1,11 +1,22 @@
 class Speaker {
-    constructor(private id: number, public name: string, public bio: string, public company: string, public social: Array<{name: string, link: string}>) {
+    private id: number;
+    public name: string;
+    public bio: string;
+    public company: string;
+    public social: Array<{name: string, link: string}>;
 
-    }
-
-    getSessions() {
-        // TODO: get the sessions of the speaker
-        //      (get the id and then the title + link to the session view when available)
+    constructor(speaker: JSON) {
+        this.id      = speaker['id'];
+        this.name    = speaker['name'];
+        this.bio     = speaker['bio'];
+        this.company = speaker['company'];
+        
+        this.social = [];
+        let socials = speaker['socials'];
+        for (let i in socials) {
+            let socialNetwork = socials[i];
+            this.social.push({name: socialNetwork['name'], link: socialNetwork['link']});
+        }
     }
 
 }
