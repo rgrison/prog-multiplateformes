da58850d2460b06b2f9438bc87d50c07e7516bf3
Rémi Grison <rl.grison@gmail.com>
2018-10-26 16:46:11 +0200

    added human readable date

--- src/pages/conferences/conferences.ts
+++ src/pages/conferences/conferences.ts
@@ -30,26 +30,35 @@ export class ConferencesPage implements OnInit {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ConferencesPage');
   }
-
+  
+  
   ngOnInit() {
     this.storage.get(Constants.SESSIONS).then(sessionsStored => {
       this.sessions = sessionsStored;
-
+      
       // sorting sessions by day, then sorting by hour
       const dates = Array.from(new Set(this.sessions.map(session => session.date)));
       let sessionsByDay = {};
       dates.forEach(date => sessionsByDay[date] = []);
-  
+      
       this.sessions.forEach(session => sessionsByDay[session.date].push(session));
-
-      // sorting sessions by beginning time
+      
+      // sorting sessions by beginning time + date in human readable format
       Object.keys(sessionsByDay).forEach(day => {
-        this.sessionsByDay[day] = sessionsByDay[day].sort((session1, session2) => session1.startTime.localeCompare(session2.startTime));
+        this.sessionsByDay[this.getReadableDate(day)] = sessionsByDay[day].sort((session1, session2) => session1.startTime.localeCompare(session2.startTime));
       });
     });
-
+    
   }
+  
+  months = {1: 'janvier', 2: 'février', 3: 'mars', 4: 'avril', 5: 'mai', 6: 'juin', 7: 'juillet', 8: 'août', 9: 'septembre', 10: 'octobre', 11: 'novembre', 12: 'décembre'};
+  getReadableDate = (date: string) => {
+    date = date.substr(5, date.length);
+    let month = Number(date.substr(0, 2));
+    let day   = date.substr(3, date.length);
 
+    return day + ' ' + this.months[month]
+  }
   days() {
     return Object.keys(this.sessionsByDay);
   }
